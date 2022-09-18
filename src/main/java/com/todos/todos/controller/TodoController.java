e00d29830d8e420832daf60035e14deb5670ae97
maksimblankov <maxblankov436@gmail.com>
2022-09-18 20:22:08 +0300

    add swagger describe for rest api

--- src/main/java/com/todos/todos/controller/TodoController.java
+++ src/main/java/com/todos/todos/controller/TodoController.java
@@ -3,6 +3,8 @@ package com.todos.todos.controller;
 import com.todos.todos.dto.TodoDTO;
 import com.todos.todos.entity.TodoEntity;
 import com.todos.todos.service.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
+@Tag(name = "Todo Controls", description = "API Endpoints to operate on Todo")
 @RequestMapping("api/v1/todo")
 public class TodoController {
 
@@ -26,12 +29,14 @@ public class TodoController {
         this.service = service;
     }
 
+    @Operation(summary = "Add todo.")
     @PostMapping
     public ResponseEntity<TodoDTO> addTodo(@RequestBody TodoDTO todo) {
         service.addTodo(todo);
         return ResponseEntity.ok(todo);
     }
 
+    @Operation(summary = "Get todo by id.")
     @GetMapping("/{id}")
     public ResponseEntity<TodoDTO> getTodo(@PathVariable Long id) {
         Optional<TodoEntity> todoEntity = service.getTodo(id);
@@ -42,6 +47,7 @@ public class TodoController {
         }
     }
 
+    @Operation(summary = "Get all todos.")
     @GetMapping
     public ResponseEntity<Collection<TodoDTO>> getTodos() {
         Collection<TodoEntity> entities = service.getTodos();
@@ -49,6 +55,7 @@ public class TodoController {
         return ResponseEntity.ok(todos);
     }
 
+    @Operation(summary = "Delete todo by id.")
     @DeleteMapping("/{id}")
     public ResponseEntity<TodoDTO> deleteTodo(@PathVariable Long id) {
 
@@ -60,6 +67,7 @@ public class TodoController {
         }
     }
 
+    @Operation(summary = "Update todo by id.")
     @PatchMapping("/{id}")
     public ResponseEntity<TodoDTO> updateDTO(@PathVariable Long id, TodoDTO todo) {
         return ResponseEntity.ok(service.update(id, todo));
